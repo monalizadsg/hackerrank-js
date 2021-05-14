fe7fd3d23f5b7ed0d0ea17d4645f2ffd989a6744
Monaliza Dasig <monalizadasig@gmail.com>
2021-05-14 15:46:07 +0800

    Add day 4 challenge

---
+++ 10_Days_of_JavaScript/classes.js
@@ -0,0 +1,15 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon {
+  constructor(args) {
+    this.args = args;
+  }
+
+  perimeter() {
+    return this.args.reduce((a, b) => a + b);
+  }
+}
